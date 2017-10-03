7627f4096ede26792acdde65f7f5192e816b1756
Nicanor <nicanor5+github@gmail.com>
2017-10-03 22:22:18 +0200

    Fixed prompt echo at the end of install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,6 @@ pip install pysptk
 sudo apt-get install praat
 git submodule init
 git submodule update
-echo "For Kaldi output Kaldi must be installed beforehand and the path at kaldi-io/kaldiio.py:line 14: os.environ['KALDI_ROOT']='/mnt/matylda5/iveselyk/Tools/kaldi-trunk' should be changed to with path to the proper Kaldi root directory.""
+echo For Kaldi output Kaldi must be installed beforehand and the path at kaldi-io/kaldiio.py:
+echo line 14: os.environ['KALDI_ROOT']='/mnt/matylda5/iveselyk/Tools/kaldi-trunk'
+echo should be changed to with path to the proper Kaldi root directory.
