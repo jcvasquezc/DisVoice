3dd9965afddc2cf7ac15bf446cbfb5141f15b755
Jabin <ngong6@gatech.edu>
2023-07-16 17:12:33 -0400

    Fix imports

--- disvoice/__init__.py
+++ disvoice/__init__.py
@@ -6,10 +6,10 @@ from .script_mananger import script_manager
 
 
 from . import (glottal, phonation, phonological, prosody, replearning, articulation)
-from .glottal.glottal import Glottal
+from .glottal.Glottal import Glottal
 from .phonation.phonation import Phonation
 from .phonological.phonological import Phonological
-from .prosody.prosody import Prosody
+from .prosody.Prosody import Prosody
 from .replearning.replearning import RepLearning
 from .articulation.articulation import Articulation
 
